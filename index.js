1d4b0d49fb19aca9638b1df54d9c33a2992841a8
prio12 <maksudurprio.mp@gmail.com>
2026-06-12 22:09:52 +0600

    solved: solved the LCM problem

---
+++ index.js
@@ -0,0 +1,41 @@
+const express = require("express");
+const app = express();
+const port = process.env.PORT || 3000;
+
+function getGCD(a, b) {
+  while (b !== 0) {
+    let temp = b;
+    b = a % b;
+    a = temp;
+  }
+  return a;
+}
+
+function getLCM(a, b) {
+  return (a * b) / getGCD(a, b);
+}
+
+function isNaturalNumber(str) {
+  if (!str || !/^\d+$/.test(str)) return false;
+  let num = parseInt(str, 10);
+  return num > 0;
+}
+
+app.get("/maksudurrahmanprio_gmail_com", (req, res) => {
+  const { x, y } = req.query;
+  if (!isNaturalNumber(x) || !isNaturalNumber(y)) {
+    res.setHeader("Content-Type", "text/plain");
+    return res.send("NaN");
+  }
+
+  const numX = parseInt(x, 10);
+  const numY = parseInt(y, 10);
+
+  const result = getLCM(numX, numY);
+  res.setHeader("Content-Type", "text/plain");
+  res.send(result.toString());
+});
+
+app.listen(port, () => {
+  console.log(` App listening on port ${port}`);
+});
